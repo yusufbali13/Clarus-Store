ded131ff8b457492d4cf5e929eccbb699439a0f4
Yusuf <baliyusuf675@gmail.com>
2023-04-08 12:14:58 +0300

    react-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import FavoritesPage from "./pages/FavoritesPage";
+import NavBar from "./components/NavBar";
 import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (
-    <div className="bg-slate-500 min-h-screen text-center">
-      <Provider></Provider>
+    <div className="bg-gray-400 min-h-screen">
+      <Provider store={store}>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
