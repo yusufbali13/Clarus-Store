db70eb525dafcb8980b561d3d6c37df757e1cbf2
Yusuf <baliyusuf675@gmail.com>
2023-04-08 11:28:21 +0300

    navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <h3 className="flex justify-between h-[50px] text-white bg-gray-500">
+        Clarus Store
+      </h3>
+      <div></div>
+    </div>
+  );
+};
+
+export default NavBar;
