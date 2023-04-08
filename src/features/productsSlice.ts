deb5a2937fd63df50002a79080dd1ab43bf5e2a0
Yusuf <baliyusuf675@gmail.com>
2023-04-08 11:47:53 +0300

    products

--- src/features/productsSlice.ts
+++ src/features/productsSlice.ts
@@ -1,28 +1,42 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "../../app/store";
+import type { RootState } from "../app/store";
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+}
 
 // Define a type for the slice state
 interface ProductsState {
-  value: number;
+  loading: boolean;
+  error: boolean;
+  favorites: Product[];
+  productsList: Product[];
 }
 
 // Define the initial state using that type
 const initialState: ProductsState = {
-  value: 0,
+  loading: false,
+  error: false,
+  favorites: [],
+  productsList: [],
 };
 
-export const counterSlice = createSlice({
-  name: "counter",
+export const productsSlice = createSlice({
+  name: "products",
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
     // Use the PayloadAction type to declare the contents of `action.payload`
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
@@ -30,9 +44,10 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } =
+  productsSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectCount = (state: RootState) => state.counter.value;
+export const selectCount = (state: RootState) => state.products.value;
 
-export default counterSlice.reducer;
+export default productsSlice.reducer;
